a4e3b9e1d7d8e92173c08135513fa213ac379291
Foti.Mougos <foti.mougos@gmail.com>
2020-04-17 16:21:54 +1000

    finished everything

--- app.js
+++ app.js
@@ -4,32 +4,97 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+async function main() {
+    const team = await getTeam();
+    const teamClassMap = mapTeamToClass(team);
+    const content = render(teamClassMap);
+    writeHTMLFile(content);
+}
+
+async function getTeam() {
+    const team = [];
+    const questions = [
+        {
+            name: "role",
+            type: "list",
+            message: "Please enter this person's role",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            name: "name",
+            type: "input",
+            message: "Please enter this person's name"
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter this person's ID number"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter this person's email"
+        }, {
+            name: "officeNumber",
+            type: "input",
+            message: "Please enter the manager's Office Number",
+            when: (answer) => answer.role === "Manager"
+        }, {
+            name: "github",
+            type: "input",
+            message: "Please enter the engineer's github username",
+            when: (answer) => answer.role === "Engineer"
+        }, {
+            name: "school",
+            type: "input",
+            message: "Please enter the name of the intern's school",
+            when: (answer) => answer.role === "Intern"
+        },
+    ];
+
+    // Get manager info
+    team.push(await inquirer.prompt(questions));
+
+    while ( true === await inquirer.prompt([{ name: "addNewMember", type: 'confirm', message: 'Would you like to add another member?'}]) ){
+        team.push(await inquirer.prompt(questions));
+    }
+
+    return team;
+}
+
+function mapTeamToClass(team){
+    return team.map((employee) => {
+        switch (employee.role){
+            case 'Manager':
+                return new manager(employee.name, employee.id, employee.email, employee.officeNumber);
+                break;
+            case 'Engineer':
+                return new engineer(employee.name, employee.id, employee.email, employee.github);
+                break;
+            case 'Intern':
+                return new intern(employee.name, employee.id, employee.email, employee.school);
+                break;
+            default:
+                //Uhhhh return nothing?
+                console.log("invalid employee role");
+                return;
+        }
+    });
+}
+
+
+function writeHTMLFile(content) {
+    fs.writeFile("output/team.html", content, function (err) {
+        if (err) 
+            throw err;
+        
+        console.log('Team Saved!');
+    });
+}
+
+main();
